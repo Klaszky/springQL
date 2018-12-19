0244e059a8a5d1ab1c3ca6133d305c47b9170c5f
Klaszky <jfklaszky@gmail.com>
2018-12-18 23:50:50 -0500

    inital commit

---
+++ src/main/java/com/klaszky/springQL/SpringQlApplication.java
@@ -0,0 +1,14 @@
+package com.klaszky.springQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQlApplication.class, args);
+	}
+
+}
+
